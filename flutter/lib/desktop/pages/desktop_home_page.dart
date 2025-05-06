5a1760db0e7fc109184148b9fa53e151e3d7e323
高以倫 <>
2025-05-06 15:58:42 +0800

    no message

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -99,7 +99,7 @@ class _DesktopHomePageState extends State<DesktopHomePage>
           child: Column(
             children: [
               Image.asset('assets/logo.png', width: 200, height: 200),
-              Obx(() => Text(_arg.value,
+              Obx(() => Text("connected......",
                   style: const TextStyle(
                     fontSize: 14,
                     color: Colors.black,
